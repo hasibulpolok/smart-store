61e8916542a96e3f2553b9b0cf4175c59cf63443
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-10 11:33:26 +0600

    sidebar

---
+++ includes/sidebar.php
@@ -0,0 +1,12 @@
+<div class="sidebar">
+
+<h4 class="text-center mt-3">Smart Store</h4>
+
+<a href="dashboard.php">Dashboard</a>
+<a href="products.php">Products</a>
+<a href="categories.php">Categories</a>
+<a href="customers.php">Customers</a>
+<a href="sales.php">Sales</a>
+<a href="../logout.php">Logout</a>
+
+</div>
