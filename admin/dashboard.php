9f91920e25bd3b39e6750617a1a8ff5d6eee2765
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-10 11:34:56 +0600

    admin dashboard update

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,14 +2,58 @@
 session_start();
 
 if(!isset($_SESSION['user_id'])){
-    header("Location: ../login.php");
+header("Location: ../login.php");
 }
 ?>
 
-<h1>Dashboard</h1>
+<?php include("../includes/header.php"); ?>
 
-Welcome <?php echo $_SESSION['name']; ?>
+<?php include("../includes/sidebar.php"); ?>
 
-<br><br>
+<div class="content">
 
-<a href="../logout.php">Logout</a>
+<h2>Dashboard</h2>
+
+<div class="row">
+
+<div class="col-md-3">
+<div class="card bg-primary text-white">
+<div class="card-body">
+<h5>Total Products</h5>
+<h3>120</h3>
+</div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="card bg-success text-white">
+<div class="card-body">
+<h5>Total Sales</h5>
+<h3>$5000</h3>
+</div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="card bg-warning text-white">
+<div class="card-body">
+<h5>Customers</h5>
+<h3>85</h3>
+</div>
+</div>
+</div>
+
+<div class="col-md-3">
+<div class="card bg-danger text-white">
+<div class="card-body">
+<h5>Low Stock</h5>
+<h3>5</h3>
+</div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+<?php include("../includes/footer.php"); ?>
