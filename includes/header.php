3d2145f79cc9f888e0fd3671a778d8c2d87ebb93
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-10 11:32:44 +0600

    header php

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Smart Store Admin</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+
+body{
+display:flex;
+}
+
+.sidebar{
+width:250px;
+height:100vh;
+background:#343a40;
+color:white;
+}
+
+.sidebar a{
+color:white;
+text-decoration:none;
+display:block;
+padding:10px;
+}
+
+.sidebar a:hover{
+background:#495057;
+}
+
+.content{
+flex:1;
+padding:20px;
+}
+
+</style>
+
+</head>
+
+<body>
