803b5a0c70aace4cede37247b0367a980e5ba8b3
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-10 11:56:22 +0600

    products/add/php

---
+++ admin/products/add.php
@@ -0,0 +1,64 @@
+<?php
+include("../../config/db.php");
+
+if(isset($_POST['save'])){
+
+$name = $_POST['name'];
+$category = $_POST['category'];
+$purchase = $_POST['purchase_price'];
+$sale = $_POST['sale_price'];
+$stock = $_POST['stock'];
+
+$image = $_FILES['image']['name'];
+$tmp = $_FILES['image']['tmp_name'];
+
+move_uploaded_file($tmp,"../../uploads/products/".$image);
+
+mysqli_query($conn,"INSERT INTO products(name,category_id,purchase_price,sale_price,stock,image)
+VALUES('$name','$category','$purchase','$sale','$stock','$image')");
+
+}
+?>
+
+<h2>Add Product</h2>
+
+<form method="post" enctype="multipart/form-data">
+
+Product Name <br>
+<input type="text" name="name"><br><br>
+
+Category <br>
+
+<select name="category">
+
+<?php
+$cat = mysqli_query($conn,"SELECT * FROM categories");
+
+while($c = mysqli_fetch_assoc($cat)){
+?>
+
+<option value="<?php echo $c['id']; ?>">
+<?php echo $c['name']; ?>
+</option>
+
+<?php } ?>
+
+</select>
+
+<br><br>
+
+Purchase Price <br>
+<input type="text" name="purchase_price"><br><br>
+
+Sale Price <br>
+<input type="text" name="sale_price"><br><br>
+
+Stock <br>
+<input type="text" name="stock"><br><br>
+
+Product Image <br>
+<input type="file" name="image"><br><br>
+
+<button name="save">Add Product</button>
+
+</form>
