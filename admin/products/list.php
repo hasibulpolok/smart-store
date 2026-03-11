d05da5a10d0f1a38dd76e0f829af8926d9643028
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-11 09:31:16 +0600

    admin/products/list

--- admin/products/list.php
+++ admin/products/list.php
@@ -1,13 +1,34 @@
 <?php
 include("../../config/db.php");
-
-$result = mysqli_query($conn,"
-SELECT products.*, categories.name AS category
-FROM products
-LEFT JOIN categories
-ON products.category_id = categories.id
-");
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Product List</title>
+
+<style>
+table{
+border-collapse: collapse;
+width: 100%;
+}
+
+th,td{
+border:1px solid black;
+padding:10px;
+text-align:center;
+}
+
+img{
+width:60px;
+}
+
+</style>
+
+</head>
+
+<body>
 
 <h2>Product List</h2>
 
@@ -15,9 +36,10 @@ ON products.category_id = categories.id
 
 <br><br>
 
-<table border="1" cellpadding="10">
+<table>
 
 <tr>
+
 <th>ID</th>
 <th>Name</th>
 <th>Category</th>
@@ -25,24 +47,44 @@ ON products.category_id = categories.id
 <th>Stock</th>
 <th>Image</th>
 <th>Action</th>
+
 </tr>
 
-<?php while($row = mysqli_fetch_assoc($result)){ ?>
+<?php
+
+$sql = "SELECT products.*, categories.name AS category
+FROM products
+LEFT JOIN categories
+ON products.category_id = categories.id";
+
+$result = mysqli_query($conn,$sql);
+
+while($row = mysqli_fetch_assoc($result)){
+
+?>
 
 <tr>
 
 <td><?php echo $row['id']; ?></td>
+
 <td><?php echo $row['name']; ?></td>
+
 <td><?php echo $row['category']; ?></td>
+
 <td><?php echo $row['sale_price']; ?></td>
+
 <td><?php echo $row['stock']; ?></td>
 
 <td>
-<img src="../../uploads/products/<?php echo $row['image']; ?>" width="60">
+
+<img src="../../uploads/products/<?php echo $row['image']; ?>">
+
 </td>
 
 <td>
+
 <a href="delete.php?id=<?php echo $row['id']; ?>">Delete</a>
+
 </td>
 
 </tr>
@@ -50,3 +92,6 @@ ON products.category_id = categories.id
 <?php } ?>
 
 </table>
+
+</body>
+</html>
