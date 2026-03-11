76bea919e2e56cb0678ccc8433ae5c2b268dd41c
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-11 10:03:52 +0600

    checkout up

--- admin/pos/checkout.php
+++ admin/pos/checkout.php
@@ -1,13 +1,16 @@
 <?php
+
 include("../../config/db.php");
+
+$customer=$_POST['customer'];
 
 $sql="SELECT * FROM cart";
 $result=mysqli_query($conn,$sql);
 
 while($row=mysqli_fetch_assoc($result)){
 
-mysqli_query($conn,"INSERT INTO sales(product_id,qty,price,total)
-VALUES('$row[product_id]','$row[qty]','$row[price]','$row[total]')");
+mysqli_query($conn,"INSERT INTO sales(product_id,qty,price,total,customer)
+VALUES('$row[product_id]','$row[qty]','$row[price]','$row[total]','$customer')");
 
 mysqli_query($conn,"UPDATE products SET stock=stock-$row[qty]
 WHERE id=$row[product_id]");
@@ -16,4 +19,6 @@ WHERE id=$row[product_id]");
 
 mysqli_query($conn,"DELETE FROM cart");
 
-echo "Sale Completed";
+header("Location:invoice.php");
+
+?>
