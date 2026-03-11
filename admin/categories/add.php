6e8b393a28ed87da3299e36978005f60776d9251
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-03-11 09:28:16 +0600

    adminadd

--- admin/categories/add.php
+++ admin/categories/add.php
@@ -1,48 +1,81 @@
 <?php
 include("../../config/db.php");
-include("../../includes/header.php");
-include("../../includes/sidebar.php");
 
-$result = mysqli_query($conn,"SELECT * FROM categories");
+if(isset($_POST['save'])){
+
+$name = $_POST['name'];
+$category = $_POST['category'];
+$purchase = $_POST['purchase_price'];
+$sale = $_POST['sale_price'];
+$stock = $_POST['stock'];
+
+$image = $_FILES['image']['name'];
+$tmp = $_FILES['image']['tmp_name'];
+
+move_uploaded_file($tmp,"../../uploads/products/".$image);
+
+mysqli_query($conn,"INSERT INTO products(name,category_id,purchase_price,sale_price,stock,image)
+VALUES('$name','$category','$purchase','$sale','$stock','$image')");
+
+echo "<p style='color:green'>Product Added Successfully</p>";
+
+}
 ?>
 
-<div class="content">
+<!DOCTYPE html>
+<html>
+<head>
+<title>Add Product</title>
+</head>
 
-<h2>Category List</h2>
+<body>
 
-<a href="add.php" class="btn btn-primary mb-3">Add Category</a>
+<h2>Add Product</h2>
 
-<table class="table table-bordered">
+<form method="post" enctype="multipart/form-data">
 
-<tr>
-<th>ID</th>
-<th>Name</th>
-<th>Action</th>
-</tr>
+Product Name <br>
+<input type="text" name="name" required>
+<br><br>
+
+Category <br>
+<select name="category">
 
 <?php
-while($row = mysqli_fetch_assoc($result)){
+$cat = mysqli_query($conn,"SELECT * FROM categories");
+
+while($c = mysqli_fetch_assoc($cat)){
 ?>
 
-<tr>
-
-<td><?php echo $row['id']; ?></td>
-<td><?php echo $row['name']; ?></td>
-
-<td>
-
-<a href="edit.php?id=<?php echo $row['id']; ?>" class="btn btn-warning btn-sm">Edit</a>
-
-<a href="delete.php?id=<?php echo $row['id']; ?>" class="btn btn-danger btn-sm">Delete</a>
-
-</td>
-
-</tr>
+<option value="<?php echo $c['id']; ?>">
+<?php echo $c['name']; ?>
+</option>
 
 <?php } ?>
 
-</table>
+</select>
 
-</div>
+<br><br>
 
-<?php include("../../includes/footer.php"); ?>
+Purchase Price <br>
+<input type="text" name="purchase_price">
+<br><br>
+
+Sale Price <br>
+<input type="text" name="sale_price">
+<br><br>
+
+Stock <br>
+<input type="text" name="stock">
+<br><br>
+
+Product Image <br>
+<input type="file" name="image">
+<br><br>
+
+<button type="submit" name="save">Add Product</button>
+
+</form>
+
+</body>
+</html>
